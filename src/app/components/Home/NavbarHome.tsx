19106db8a4045ef770f4e7662a66c97742bfe8b8
SarCraft <nathan.henaux30@gmail.com>
2023-12-16 11:11:16 +0100

    import shadcn component

--- src/app/components/Home/NavbarHome.tsx
+++ src/app/components/Home/NavbarHome.tsx
@@ -3,9 +3,9 @@ function NavbarHome() {
     <section>
         <div className="flex items-center max-sm:justify-center sm:justify-end mx-auto w-2/4">
             <ul className="flex flex-row justify-end text-white gap-2 p-2">
-            <a href="/#about" className="block hover:text-white hover:opacity-40 transition-all">
+            <a href="/#about" className="block opacity-40 hover:opacity-100 transition-all">
                 <code>/about</code></a>
-                <a href="/" className="block opacity-40 transition-all cursor-not-allowed">
+                <a href="/" className="block opacity-40 hover:opacity-100 transition-all">
                 <code>/project</code> 
                 </a>
                 <a href="#SkillHome" className="block opacity-40 hover:opacity-100 transition-all">
