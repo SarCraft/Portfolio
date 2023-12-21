b21962ac22e7526b79be1da6e9be7891a6e909bc
SarCraft <nathan.henaux30@gmail.com>
2023-12-21 09:32:01 +0100

    Update technos.ts with correct type names

--- config/Techno/technos.ts
+++ config/Techno/technos.ts
@@ -1,6 +1,6 @@
-import { techno } from "./technos.type";
+import { Techno } from "./technos.type";
 
-export const Figma: techno = {name: "Figma", pathImage: "/icon/figma.png", url: "https://www.figma.com/"};
-export const VScode: techno = {name: "VScode", pathImage: "/icon/vscode.png", url: "https://code.visualstudio.com/"};
-export const AdobeIllustrator: techno = {name: "Adobe Illustrator", pathImage: "/icon/adobeillustrator.jpg", url: "https://www.adobe.com/fr/products/illustrator.html"};
-export const Unity: techno = {name: "Unity", pathImage: "/icon/unity.png", url: "https://unity.com/"};
+export const Figma: Techno = {name: "Figma", pathImage: "/icon/figma.png", url: "https://www.figma.com/"};
+export const VScode: Techno = {name: "VScode", pathImage: "/icon/vscode.png", url: "https://code.visualstudio.com/"};
+export const AdobeIllustrator: Techno = {name: "Adobe Illustrator", pathImage: "/icon/adobeillustrator.jpg", url: "https://www.adobe.com/fr/products/illustrator.html"};
+export const Unity: Techno = {name: "Unity", pathImage: "/icon/unity.png", url: "https://unity.com/"};
