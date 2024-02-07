3b26f3a2e0c7e488a8634dda5c0d62dddb8d6f05
Henaux Nathan <59655746+SarCraft@users.noreply.github.com>
2024-02-07 19:30:16 +0000

    Refactor project filtering in ProjectList component

--- src/app/components/Projets/ProjectList.tsx
+++ src/app/components/Projets/ProjectList.tsx
@@ -10,7 +10,7 @@ function ProjectList() {
                 <h2 className="text-2xl w-1/2 text-gray-600 mt-16">Fullstack</h2><Separator />
             </div>
             <div className="flex flex-row flex-wrap gap-5 text-xl pt-3 justify-start mt-6 mx-16">
-                {projects.filter(project => project.type.includes('Fullstack')).map((project, idx) => (
+                {projects.filter(project => project.type === 'Fullstack').map((project, idx) => (
                     <CardProject key={idx} name={project.name} lien={project.lien} open={project.open} date={project.date} description={project.description} skills={project.skills || []} technos={project.technos || []} />
                 ))}
             </div>
@@ -18,7 +18,7 @@ function ProjectList() {
                 <h2 className="text-2xl w-1/2 text-gray-600 mt-16">UI/UX Design</h2><Separator />
             </div>
             <div className="flex flex-row flex-wrap gap-5 text-xl pt-3 justify-start mt-6 mx-16">
-                {projects.filter(project => project.type.includes('Design')).map((project, idx) => (
+                {projects.filter(project => project.type ==='Design').map((project, idx) => (
                         <CardProject key={idx} name={project.name} lien={project.lien} open={project.open} date={project.date} description={project.description} skills={project.skills || []} technos={project.technos || []} />
                 ))}
             </div>
@@ -26,7 +26,7 @@ function ProjectList() {
                 <h2 className="text-2xl w-1/2 text-gray-600 mt-16">Other</h2><Separator />
             </div>
             <div className="flex flex-row flex-wrap gap-5 text-xl pt-3 justify-start mt-6 mx-16">
-                {projects.filter(project => project.type.includes('Other')).map((project, idx) => (
+                {projects.filter(project => project.type === 'Other').map((project, idx) => (
                         <CardProject key={idx} name={project.name} lien={project.lien} open={project.open} date={project.date} description={project.description} skills={project.skills || []} technos={project.technos || []} />
                 ))}
             </div>
